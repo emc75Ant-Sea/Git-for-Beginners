3260fa070d96d30b2e7ba98e9df3c553eee99c30
Emmanuel Cartal <emc75@hotmail.com>
2026-08-14 14:09:38 +0200

    added substract_nums function and updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, substract_nums
 
 if __name__ == "__main__":
     print(add_nums(1,3))
+    print(substract_nums(5,3))
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_nums(a,b):
         return a+b
+
+def substract_nums(a,b):
+        return a-b
