ebcbeba2b5df2e712bb2b7ff5969061e2a8ce66f
Emmanuel Cartal <emc75@hotmail.com>
2026-08-24 16:29:42 +0200

    add descriptive labels to print statement on main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.calculator import add_nums, divide_nums, substract_nums, multiply_nums, power
 
 if __name__ == "__main__":
-    print(add_nums(1,3))
-    print(divide_nums(5,3))
-    print(substract_nums(5,3))
-    print(multiply_nums(3,4))
-    print(power(4,2))
+    print("Addition:", add_nums(1,3))
+    print("Division:", divide_nums(5,3))
+    print("Substraction", substract_nums(5,3))
+    print("Multiplication", multiply_nums(3,4))
+    print("Power", power(4,2))
